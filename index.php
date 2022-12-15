ef476304fb2c897283e1ce281e0d57b89c8e9cdf
lankeycode <michael.moussaoui@gmail.com>
2022-12-15 17:14:20 +0100

    start home page

--- index.php
+++ index.php
@@ -1,20 +1,55 @@
 <?php
+
+// connect to database
+
+// User connected
+
+// On stocke les informations du user dans 
+
+$user = [
+    "id" => 1,
+    "name" => "michael",
+    "age" => 43,
+    "genre" => "homme",
+    "weight" => 65,
+    "size" => 183,
+    "IMC" => 19.4,
+    "email" => "michael.moussaoui@gmail.com",
+    "isLogged" => true
+];
+
+if (!$user["isLogged"]) {
+    header("location:login.php");
+    exit;
+}
 $page = [
     "title" => "alimentation"
 ];
 include_once("includes/header.php");
 ?>
-<div class="dropdown">
-    <h1>HELLO</h1>
-    <button class="btn btn-primary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-        Dropdown
+<div class="container">
+    <header>
+        <div class="title">Mon alimentation</div>
+        <div class="profile"> <?php echo $user['name']; ?></div>
+    </header>
+    <section class="dataUser">
+        <div>Graph</div>
+        <div>IMC</div>
+        <div><?php echo $user['weight']; ?> Kg</div>
+    </section>
+    <section class="date">
+        <div> <?php echo date("l d M Y"); ?></div>
 
-    </button>
-    <ul class="dropdown-menu">
-        <li><a class="dropdown-item" href="#">Dropdown item</a></li>
-        <li><a class="dropdown-item" href="#">Dropdown item</a></li>
-        <li><a class="dropdown-item" href="#">Dropdown item</a></li>
-    </ul>
+    </section>
+    <section class="list">
+        <div class="food">
+            <div class="titleFood">Big Mac</div>
+            <div class="kgFood">504 kcal</div>
+        </div>
+    </section>
+    <footer>
+        <button>+</button>
+    </footer>
 </div>
 <?php
 include_once("includes/footer.php");
--- login.php
+++ login.php
@@ -1,4 +1,9 @@
 <?php
+
+
+
+
+
 $page = [
     "title" => "login"
 ];
